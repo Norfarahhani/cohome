4a89c7a36ef9f37ceaca18b0a722818362146b4f
Norfarahhani <farahhani0672@gmail.com>
2024-09-10 21:16:40 +0800

    menu

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, MenuComponent, LayoutComponent],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, MenuComponent, LayoutComponent]
 })
 export class ComponentsModule { }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { library, playCircle, radio, search } from 'ionicons/icons';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class FooterComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class MenuComponent {
+  constructor() {
+    addIcons({ library, playCircle, radio, search });
+  }
 }
