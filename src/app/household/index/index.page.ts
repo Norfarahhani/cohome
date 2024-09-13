da70c0c785a9ddaac26385efc15d3bd850c57f86
zabuzaff <zabuzaff@gmail.com>
2024-09-13 20:04:50 +0800

    fix

--- src/app/household/index/index.page.ts
+++ src/app/household/index/index.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: 'app-household-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
 })
--- src/app/profile/index/index.page.ts
+++ src/app/profile/index/index.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: 'app-profile-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
 })
