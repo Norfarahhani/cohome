d15e556c3fc30bf6d632287303cbf2c3183e88d6
Norfarahhani <farahhani0672@gmail.com>
2024-09-17 16:36:44 +0800

    new task modal

--- src/app/task/create/create.page.ts
+++ src/app/task/create/create.page.ts
@@ -8,11 +8,21 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreatePage implements OnInit {
 
+  repeatReminder: boolean = false; // Toggle state
+  repeatTime: string = ''; // Initialize with an empty string
+  selectedRepeatOption: string = ''; // Selected repeat option
+  selectedMember: string=''; // assign task kepada siapa
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleReminder(event: any) {
+    // Logic to handle toggle change
+    console.log('Repeat Reminder:', this.repeatReminder);
+  }
 }
+
 
 
