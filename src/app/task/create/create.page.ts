6e1db89e8205c74dcad34610c64740e0f18ae63b
zabuzaff <zabuzaff@gmail.com>
2024-09-17 15:28:06 +0800

    task create page example

--- src/app/task/create/create.page.ts
+++ src/app/task/create/create.page.ts
@@ -2,27 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-create',
+  selector: 'app-task-create',
   templateUrl: './create.page.html',
   styleUrls: ['./create.page.scss'],
 })
 export class CreatePage implements OnInit {
 
-  constructor(private modalCtrl: ModalController) {}
-
-  // Method to save the task
-  saveTask() {
-    // Add your task-saving logic here, e.g., form submission, validation, etc.
-    console.log('Task saved!');
-
-    // Optionally dismiss the modal after saving
-    this.modalCtrl.dismiss();
-  }
-
-  // Method to dismiss the modal
-  dismissModal() {
-    this.modalCtrl.dismiss();
-  }
+  constructor() { }
 
   ngOnInit() {
   }
