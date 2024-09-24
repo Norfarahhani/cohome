94f0f74cd1392eb4c3c038e0737167e50a35ad33
Norfarahhani <farahhani0672@gmail.com>
2024-09-24 12:27:30 +0800

    create changes

--- src/app/expense/expense-routing.module.ts
+++ src/app/expense/expense-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     component: UnpaidPage
   },
   {
-    path: '',
+    path: 'edit',
     component: EditPage
   }
 ];
--- src/app/expense/unpaid/unpaid.page.ts
+++ src/app/expense/unpaid/unpaid.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnpaidPage implements OnInit {
 
-    constructor() { }
+    constructor(private modalCtrl: ModalController) { }
 
     ngOnInit() {
     }
